bfb2d49d05da527f8d75eb19523a815de2cf6832
Alexis Guzman <spartanthelexvro@gmail.com>
2024-10-02 20:16:29 -0600

    Registro de libros bien

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { iBook } from '../books/models/iBook';
+import { NewBookService } from './service/new-book.service';
+import Swal from 'sweetalert2'
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-registration-page',
@@ -15,12 +18,33 @@ export class RegistrationPageComponent {
     id_literary_genre: 0
   };
 
+  constructor(private _POSTbook: NewBookService) {}
+
   onFormBooksDataReceived(data: iBook):void {
     this.formDataBooks = data;
-    console.log(this.formDataBooks);
-  }
+    console.log("Esta es la data", this.formDataBooks);
 
-  saveDataBook():void {
-    console.log(this.formDataBooks);
+    this._POSTbook.postBook(this.formDataBooks).subscribe(
+      response => {
+        console.log('Libro guardado con éxito:', response);
+        Swal.fire({
+          icon: "success",
+          title: "Good",
+          text: "Libro guardado",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      },
+      error => {
+        console.error('Error al guardar el libro:', error);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Ocurrió un error!",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    );
   }
 }
---
+++ src/app/registration-page/service/new-book.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { iBook } from '../models/iBook';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewBookService {
+  private url_base = 'http://localhost:3000/books/add';
+
+  constructor(private _http: HttpClient) {}
+
+  postBook(book: iBook): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this._http.post(this.url_base, book, { headers });
+  }
+}
